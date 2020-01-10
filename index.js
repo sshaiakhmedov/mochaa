2167aa8b54d8c1487f13449f17a4f2650d5e8c95
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-01-10 12:16:16 -0700

    assertion with mocha and chai libraries

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ function sum (a, b){
 function prod (a, b){
     return a * b;
 }
-module.exports = {sum, prod, even, isAdult, sortAtoZ, isPalindrome, oddNumber};
+module.exports = {sum, prod, isEven, isAdult, sortAtoB, isPalindrome, oddNumber};
 
-
-function even (arr){
+/*The function is given a random array arr of numbers .
+It shall return a new array arrEven with even numbers only from arr array.*/
+function isEven (arr){
     let arrEven=[];
     arrEven=arr.filter(el=>el%2==0);
     return arrEven;
 }
-
+/*Function assess an input c for entries as:
+<0 value
+string value
+String number value
+String number with spaces value, so state whether a person is Adult or not (>18)*/
 function isAdult (c){
-    return c>18? true : false;
+    return c>18 && c!==/\d\s/? true : false;
 }
-
-function sortAtoZ (arr) {
-    return arr.sort();
+/*The function is given a random array arr of numbers or string charters.
+It shall return the same array sorted in alphabetical or numbers in ascending order.
+If any element in array is empty==undefined function shall throw 'Error'*/
+function sortAtoB (arr) {
+    if (arr.includes(undefined)) throw 'Error';
+    return  arr.sort();
 }
 
 function isPalindrome (word) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const {expect} = require('chai');
-const {sum, prod, even, isAdult, sortAtoZ, isPalindrome, oddNumber} = require('../index.js');
+const {sum, prod, isEven, isAdult, sortAtoB, isPalindrome, oddNumber} = require('../index.js');
 
 describe('function sum',()=>{
     it('should variable a equals 10', ()=>{
@@ -18,17 +18,31 @@ it('should function prod equals 15', ()=>{
 });
 
 it('function shall return new array with even number', ()=>{
-    assert.deepEqual(even ([4,52,35,6]), [4,52,6]);
+    assert.deepEqual(isEven ([4,52,35,6]), [4,52,6]);
 });
 
-
-it('function shall return true if entry is >21', ()=> {
+/*The Unit test assess the input c for entries as:
+c>18
+c<18
+String, string number value or string number value with spaces, to state whether a person is Adult or not (>18)*/
+it('function shall return true if a Person age c>18 and false if c<18', ()=> {
     assert.equal(isAdult(19),true);
+    assert.equal(isAdult(16), false);
+    assert.deepEqual(isAdult(' 22 '), true);
     expect(isAdult(19)).true;
+    expect(isAdult(16)).false;
+    expect(isAdult('er')).false;
 });
 
-it('function shall return array sorted in alphabetical order: a to z ', ()=> {
-    assert.deepEqual(sortAtoZ(['b', 's', 'e','z','f']), ['b','e','f','s','z'])
+/*This Unit test is given a random array arr of numbers or string charters.
+It shall return the same array sorted in alphabetical or numbers in ascending order.
+If any element in array is empty==undefined function shall throw 'Error'*/
+
+it('function shall return array sorted in alphabetical order(a-z) or numbers in ascending order(0-9)', ()=> {
+    assert.deepEqual(sortAtoB(['b', 's', 'e','z','f']), ['b','e','f','s','z']);
+    assert.deepEqual(sortAtoB([4,3,2,1,5]), [1,2,3,4,5]);
+    assert.deepStrictEqual(sortAtoB([,3,2,1,5]), undefined, Error); //cand find the right assertion method for Error
+    expect(sortAtoB(['b', 's', 'e','z','f'])).deep.equal(['b','e','f','s','z'])
 });
 
 describe('define is the word palindrom',()=> {

4d8e0b80b96bee5bce9a16c74d2c9bd52d76f6c9
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-01-09 21:20:12 -0700

    chai assert methods .gitignore created

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,11 @@
 const assert = require('assert');
+const {expect} = require('chai');
 const {sum, prod, even, isAdult, sortAtoZ, isPalindrome, oddNumber} = require('../index.js');
 
 describe('function sum',()=>{
     it('should variable a equals 10', ()=>{
-        assert.equal(sum(3,5),8);
+         assert.equal(sum(3,5),8);
+        expect(sum(2,4)).eq(6);
     });
 
     it('should function sum not equals 0', ()=>{
@@ -21,7 +23,8 @@ it('function shall return new array with even number', ()=>{
 
 
 it('function shall return true if entry is >21', ()=> {
-    assert.equal(isAdult(19),true)
+    assert.equal(isAdult(19),true);
+    expect(isAdult(19)).true;
 });
 
 it('function shall return array sorted in alphabetical order: a to z ', ()=> {
@@ -45,7 +48,8 @@ describe('is the given number odd or not (true or false)',()=> {
     });
 
     it('function shall assert is the given number Odd-return false', () => {
-        assert.equal(oddNumber (6), false)
+        assert.equal(oddNumber (6), false);
+        expect(oddNumber(6)).false;
     });
 
 });
